921333646c96538fb91fae547bb4fdf20937de10
mailmanindinc <mailmanindinc@users.noreply.github.com>
2019-12-17 21:52:53 -0500

    Update Constants.java

--- src/test/java/org/merchant/test/common/Constants.java
+++ src/test/java/org/merchant/test/common/Constants.java
@@ -2,6 +2,6 @@ package org.merchant.test.common;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://ec2-54-174-213-136.compute-1.amazonaws.com:8080/admin";
+    public static final String BASE_URL = "http://qainterview.merchante-solutions.com:8080/admin";
 
 }
